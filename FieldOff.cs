1f373b847566d92573615f15208994763c287651
Ric Gaudet <ric.gaudet@nmlegis.gov>
2019-02-23 16:06:38 -0700

    Merge Functions Basic Parsing

---
+++ FieldOff.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WP_Reader
+{
+    public class FieldOff:MergeGroupFunction
+    {
+        public FieldOff(WP6Document doc, int index)
+            : base(doc, index)
+        {
+
+        }
+
+    }
+}
--- Functions/VariableLengthFunctions/222 (Merge)/ParseMergeGroupFunction.cs
+++ Functions/VariableLengthFunctions/222 (Merge)/ParseMergeGroupFunction.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace WP_Reader
 {
-    class ParseMergeGroupFunction
+    public static class ParseMergeGroupFunction
     {
         public static MergeGroupFunction getFunction(WP6Document doc, int index)
         {
@@ -17,14 +17,19 @@ namespace WP_Reader
                 MergeGroup name = (MergeGroup)WP6_FunctionNames.map[key];
                 switch (name)
                 {
+                    case MergeGroup.field_on:
 
+                        break;
+                    case MergeGroup.field_off:
+
+                        break;
 
                     default:
                         return new MergeGroupFunction(doc, index);
                 }
 
             }
-            return new MergeGroupFunction(doc, index);   // return empty page group function if not in name list
+            return new MergeGroupFunction(doc, index);   // return empty merge group function if not in name list
         }
 
     }
